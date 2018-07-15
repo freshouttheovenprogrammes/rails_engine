65a1b77043057d46ce1a9236f5b7260cb7c73a88
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-07-15 17:02:11 -0600

    commit take break

--- app/controllers/api/v1/merchants/searches_controller.rb
+++ app/controllers/api/v1/merchants/searches_controller.rb
@@ -1,4 +1,19 @@
-class SearchesController < ApplicationController
+class Api::V1::Merchants::SearchesController < ApplicationController
   def show
+    # require "pry"; binding.pry
+    # search do
+      # require "pry"; binding.pry
+      format.json  { render json: (Merchant.where name: params["name"]) }
+    # end
+    # require "pry"; binding.pry
+  end
+
+  private
+
+  def search(&block)
+    # require "pry"; binding.pry
+      yield if block_given?
+
+
   end
 end
--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,12 +1,15 @@
 require "test_helper"
 
 class Api::V1::MerchantsController::SearchesController < ActionDispatch::IntegrationTest
-  test "can find merchant by name parameter" do
-    create_list(:merchant, 3)
+  test "can find merchant by name parameter exact match" do
+    merchants = create_list(:merchant, 3)
+    desired_merchant = merchants[2]
 
-    get "/api/v1/merchants?find?q=bob_3"
+    get "/api/v1/merchants/find?name=Bob_3"
+
+    result = JSON.parse(response.body)
 
     assert_response :success #what kind of 200 would this be?
-
+    assert_equal result["name"], desired_merchant.name
   end
 end
