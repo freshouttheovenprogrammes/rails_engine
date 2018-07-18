22bf07b44df34483092c2906502955870928161d
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-07-18 16:29:51 -0600

    mostly configure database cleaner with minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
 require 'simplecov'
 SimpleCov.start
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path("../../config/environment", __FILE__)
+
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/rails/capybara"
-require "minitest/reporters"
+require "minitest/pride"
 
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
@@ -22,12 +24,15 @@ end
 # require "minitest/rails/capybara"
 
 # Uncomment for awesome colorful output
-require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   ActiveRecord::Migration.check_pending!
   fixtures :all
   include FactoryBot::Syntax::Methods
+  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.logger = Rails.logger
+  setup { DatabaseCleaner.start }
+  teardown  { DatabaseCleaner.clean }
   # Add more helper methods to be used by all tests here...
 end
